bbddc33b4f82b22fc293483fde602e912d494735
SomethingSomethingOrOther <abdoulgdia@gmail.com>
2023-08-21 12:26:38 -0400

    count dracula

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import {useState,useEffect} from "react"
 import './App.css';
 
 function App() {
+  const [rangeInput,setRangeInput]=useState(1)
+  const [intervalId,setIntervalId] = useState(null)
+  const [width,setWidth]=useState(0)
+
+ 
+
+
+   const handleStart=()=>{
+         
+            const interval=setInterval(()=>{
+              setWidth(prev=>prev >=1365 ? 0:prev + 10)
+           },100/rangeInput)
+        setIntervalId(interval)            
+          
+     }
+
+     const handlePause=()=>{
+        clearInterval(intervalId)
+        setIntervalId(null)
+     }
+
+     const handleReset=()=>{
+        setRangeInput(1)
+        clearInterval(intervalId)
+        setIntervalId(null)
+        setWidth(0)
+
+
+     }
+    
+     useEffect(() => {
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, [intervalId]);
+    
+    useEffect(() => {
+      if (rangeInput === 10) {
+        clearInterval(intervalId); 
+        setIntervalId(null);
+      }
+    }, [rangeInput, intervalId]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Progressbar</h1>
+      </div>
+      <div style={{border:"1px solid black",height:"5rem",borderRadius:"5rem"}}>
+      <div style={{width:width,maxWidth:"inherit",backgroundColor:"red",marginBottom:"2rem",border:"1px solid grey",height:"5rem",borderRadius:"5rem"}}>
+
+      </div>
+      </div>
+      <div>
+        <button onClick={handleStart}style={{cursor:"pointer",margin:"1rem",borderRadius:"3rem",border:"none",padding:"1rem 2rem",backgroundColor:"purple",color:"orange"}}>Start</button>
+        <button onClick={handlePause}style={{cursor:"pointer",margin:"1rem",borderRadius:"3rem",border:"none",padding:"1rem 2rem",backgroundColor:"purple",color:"orange"}}>Pause</button>
+        <button onClick={handleReset}style={{cursor:"pointer",margin:"1rem",borderRadius:"3rem",border:"none",padding:"1rem 2rem",backgroundColor:"purple",color:"orange"}}>Reset</button>
+      </div>
+      <div>
+        <input value={rangeInput}
+        onChange={(e)=>setRangeInput(e.target.value)}
+        type="range"min="1" max="10"/>
+          <p>{rangeInput}</p>
+      </div>
     </div>
   );
 }
